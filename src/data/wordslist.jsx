6a45f073663c23f4f23809feee319a70949bd408
nobugsatcoding <nobugsatcoding@gmail.com>
2024-03-07 18:32:26 +0200

    started raect npm and addded data with 10 words

---
+++ src/data/wordslist.jsx
@@ -0,0 +1,12 @@
+const wordsList = [
+  { id: 1, eng: 'in town', lt: 'mieste' },
+  { id: 2, eng: 'where is?', lt: 'Kur yra?' },
+  { id: 3, eng: 'hospital', lt: 'ligoninė' },
+  { id: 4, eng: 'hotel', lt: 'viešbutis' },
+  { id: 5, eng: 'supermarket', lt: 'prekybos centras' },
+  { id: 6, eng: 'toy shop', lt: 'žaislų parduotuvė' },
+  { id: 7, eng: 'school', lt: 'mokykla' },
+  { id: 8, eng: 'police station', lt: 'policijos nuovada' },
+  { id: 9, eng: 'park', lt: 'parkas' },
+  { id: 10, eng: 'shall we go to the shop?', lt: 'ar mes eisime į parduotuvę' },
+];
